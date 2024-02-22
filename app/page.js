9a49648b24c7ba246f12767c5703b92772b25274
jerome-shiftleft <jerome@theshiftleft.com>
2024-02-23 03:10:14 +0800

    progress

--- app/page.js
+++ app/page.js
@@ -1,21 +1,24 @@
 import Link from "next/link";
+import classes from "./page.module.css";
 
-export default function Home() {
-  console.log('test123');
+export default function Home() {  
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meal</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className="slideshow">          
+        </div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 }
