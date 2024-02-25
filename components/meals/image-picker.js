6f0806d30b5fd80d94df99c9816ac94c1cc75743
jerome-shiftleft <jerome@theshiftleft.com>
2024-02-25 23:50:12 +0800

    progress

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,52 +1,65 @@
 'use client';
-import { useState, useRef } from "react";
-import Image from "next/image";
-import classes from "./image-picker.module.css";
+
+import { useRef, useState } from 'react';
+import Image from 'next/image';
+
+import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
   const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
-  function handlePickClick() {    
+  function handlePickClick() {
     imageInput.current.click();
   }
 
   function handleImageChange(event) {
     const file = event.target.files[0];
-    
+
     if (!file) {
-      setPickedImage(null)
       return;
     }
+
     const fileReader = new FileReader();
+
     fileReader.onload = () => {
       setPickedImage(fileReader.result);
     };
+
     fileReader.readAsDataURL(file);
   }
 
   return (
-    <>
-      <div className={classes.picker}>
-        <label htmlFor="image">{label}</label>
-        <div className={classes.controls}>
-          <div className={classes.preview}>
-           
-            {pickedImage ? <Image src={pickedImage} alt="The image selected by the user" fill /> : <p>No image picked yet</p>}
-          </div>
-          <input
-            ref={imageInput}
-            className={classes.input}
-            type="file"
-            id="image"
-            accept="image/png, image/jpeg"
-            name={name}
-            onChange={handleImageChange}
-            required
-          />
-          <button className={classes.button} type="button" onClick={handlePickClick}>Pick an image</button>
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
         </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
       </div>
-    </>
+    </div>
   );
 }
