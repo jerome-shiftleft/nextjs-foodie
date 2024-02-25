e6b02fdabe33c78746ff7a9d63c58a24049c3d56
jerome-shiftleft <jerome@theshiftleft.com>
2024-02-25 16:44:11 +0800

    progress

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,26 @@
+import Image from 'next/image';
+import classes from './page.module.css'
+
 export default function MealDetailsPage() {
   return (
     <>
-      <p>Meal Details</p>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${`EMAIL`}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: '...',
+        }}></p>
+      </main>
     </>
   );
 }
