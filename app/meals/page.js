63559d43bff8d695af60c5994b71f36b30d24330
jerome-shiftleft <jerome@theshiftleft.com>
2024-02-25 16:17:22 +0800

    suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,28 +1,33 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import MealsGrid from "./meals-grid";
 import { getMeals } from "@/lib/meals";
 import classes from "./page.module.css";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created <span className={classes.highlight}>by you</span>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
         </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
-          <Link href="/meals/share">
-            Share Your Favorite Recipe
-          </Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
