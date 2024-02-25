d20f1e4b7671f86ddc53c5d64dcc53b633870a2c
jerome-shiftleft <jerome@theshiftleft.com>
2024-02-25 20:04:44 +0800

    progress

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, '<br />');
   return (
